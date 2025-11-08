0ac588d3211e5fdec58f3b143f24261b0c0b8405
NTT <thanhtai5223@gmail.com>
2025-11-08 21:38:54 +0700

    infra provision fix version

--- main.tf
+++ main.tf
@@ -12,12 +12,102 @@ resource "aws_security_group" "infra_sg" {
     description = "Allow SSH access"
   }
 
+   # Outbound: allow all
   egress {
+    description = "Allow all outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+# HTTP
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    # HTTPS
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    # Jenkins / web apps
+  ingress {
+    description = "Custom TCP 3000-10000"
+    from_port   = 3000
+    to_port     = 10000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   # Kubernetes NodePort range
+  ingress {
+    description = "K8s NodePort"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   # Kubernetes API server
+  ingress {
+    description = "K8s API server"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   # DevOps node (specific SG)
+  ingress {
+    description = "DevOps nodes TCP 10250"
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    security_groups = ["sg-04b67c9536ae53c17"]
+  }
+
+    # SMTP
+  ingress {
+    description = "SMTP"
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   # SMTPS
+  ingress {
+    description = "SMTPS"
+    from_port   = 465
+    to_port     = 465
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+  description     = "Allow ICMP from SG internal"
+  from_port       = -1
+  to_port         = -1
+  protocol        = "icmp"
+  self            = true
+}
+
+egress {
+  description     = "Allow all outbound ICMP"
+  from_port       = -1
+  to_port         = -1
+  protocol        = "icmp"
+  cidr_blocks     = ["0.0.0.0/0"]
+}
 
   tags = {
     Name = "infra-sg"
